e4adc20ae6858a2af0b877239d6404f6d25a5260
Jordan Stewart <149850673+Jasz3228@users.noreply.github.com>
2025-11-25 14:23:01 -0500

    Finished up part 4 of DOM crash course

--- dom.js
+++ dom.js
@@ -90,7 +90,7 @@
 
 // -- PART 2 -- TRAVERSING THE DOM
 
-const itemList = document.querySelector("#items");
+// const itemList = document.querySelector("#items");
 // parentNode
 // console.log(itemList.parentNode);
 // itemList.parentNode.style.backgroundColor = "#f4f4f4";
@@ -192,8 +192,8 @@ const itemList = document.querySelector("#items");
 
 // }
 
-const button = document.getElementById("button");
-const box = document.getElementById("box");
+// const button = document.getElementById("button");
+// const box = document.getElementById("box");
 
 
 // button.addEventListener("click", runEvent);
@@ -243,3 +243,74 @@ const box = document.getElementById("box");
 
 //   // box.style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+", 40)";
 // }
+
+// ------- PART 4 ------- Final project
+
+const form = document.getElementById("addForm");
+const itemList = document.getElementById("items");
+const filter = document.getElementById("filter");
+
+// Form submit event
+form.addEventListener("submit", addItem);
+// Delete event
+itemList.addEventListener("click", removeItem);
+// Filter event
+filter.addEventListener("keyup", filterItems);
+
+// Add item
+function addItem(e){
+  e.preventDefault();
+
+  // Get input value
+  const newItem = document.getElementById("item").value;
+
+  // Create new li element
+  const li = document.createElement("li");
+  // Add class
+  li.className = "list-group-item";
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+
+  // Create del button element
+  const deleteBtn = document.createElement("button");
+
+  // Add classes to del button
+  deleteBtn.className = "btn btn-danger btn-sm float-right delete";
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode("X"));
+
+  // Append button to li
+  li.appendChild(deleteBtn);
+
+  // Append li to list
+  itemList.appendChild(li);
+
+}
+
+
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains("delete")){
+    if(confirm("Are You Sure?")){
+      let li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
+// Filter Items
+function filterItems(e){
+  // Convert text to lowercase
+  let text = e.target.value.toLowerCase();
+  // Get list
+  let items = itemList.getElementsByTagName("li");
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    let itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
